045feffc8fab9d72760ce55c9d68d11e1802597c
schillerandrew <schiller.andrew@gmail.com>
2022-05-05 13:41:18 -0700

    rendered Yelp data on Faves.js, more styling and changes

--- src/Components/Faves.js
+++ src/Components/Faves.js
@@ -32,7 +32,7 @@ class Faves extends React.Component {
       // console.log(this.props.auth0);
       const res = await this.props.auth0.getIdTokenClaims();
       const jwt = res.__raw;
-      // console.log('jwt=' + jwt);
+      // console.log('jwt', jwt);
       const config = {
         method: 'get',
         baseURL: process.env.REACT_APP_SERVER,
@@ -53,222 +53,211 @@ class Faves extends React.Component {
     }
   }
 
-  hideDeleteModalHandler = () => {
-    this.setState({
-      showDelete: false
-    });
-  }
+  // hideDeleteModalHandler = () => {
+  //   this.setState({
+  //     showDelete: false
+  //   });
+  // }
 
-  showDeleteModalHandler = () => {
-    this.setState({
-      showDelete: true,
-    });
-  }
+  // showDeleteModalHandler = () => {
+  //   this.setState({
+  //     showDelete: true,
+  //   });
+  // }
 
-  hideUpdateModalHandler = () => {
-    this.setState({
-      showUpdate: false,
-    });
-  }
+  // hideUpdateModalHandler = () => {
+  //   this.setState({
+  //     showUpdate: false,
+  //   });
+  // }
 
-  showUpdateModalHandler = () => {
-    this.setState({
-      showUpdate: true,
-    });
-  }
+  // showUpdateModalHandler = () => {
+  //   this.setState({
+  //     showUpdate: true,
+  //   });
+  // }
 
-  hidePhotoModalHandler = () => {
-    this.setState({
-      showPhoto: false,
-    });
-  }
+  // hidePhotoModalHandler = () => {
+  //   this.setState({
+  //     showPhoto: false,
+  //   });
+  // }
 
-  showPhotoModalHandler = () => {
-    this.setState({
-      showPhoto: true,
-    });
-  }
+  // showPhotoModalHandler = () => {
+  //   this.setState({
+  //     showPhoto: true,
+  //   });
+  // }
 
   render() {
+    console.log(this.state.Reviews);
     return (
       <>
-        <Button onClick={this.getFavesAndReviews}>GET</Button>
+        <Button
+        onClick={this.getFavesAndReviews}
+        className="button"
+        >Show My Faves!</Button>
         {this.state.Faves.length ?
           this.state.Faves.map((data, id) => {
             console.log(data);
             return (
-              <p key={id}>{data.name}</p>
+              <>
+                <Container className="container">
+                  <Card style={{ width: '40rem' }}>
+                    <Card.Header className="restaurantHeader">
+                      <Row>
+                        <Col>
+                          {/* restaurant PHOTO */}
+                          <Card.Img
+                            variant="left"
+                            src={data.image_url}
+                            className="restPhoto"
+                          />
+                        </Col>
+                        <Col>
+                          {/* restaurant NAME */}
+                          <Card.Title
+                            className="restName">{data.name}</Card.Title>
+
+                          {/* restaurant ADDRESS */}
+                          <Card.Text className="restAddy">
+                            <i className="fa fa-map-marker"></i> {data.location.display_address[0]}
+                          </Card.Text>
+
+                          {/* this button shares*/}
+                          {/* <Button
+                            variant="info"
+                            className="share"
+                          >
+                            <i className="fa fa-share"></i>
+                          </Button> */}
+
+                          {/* this button deletes a restaurant */}
+                          <Button
+                            onClick={this.showDeleteModalHandler}
+                            className="deleteRest"
+                          >
+                            <i className="fa fa-trash-o"></i>
+                          </Button>
+                        </Col>
+                      </Row>
+                    </Card.Header>
+                  </Card>
+                </Container>
+              </>
             )
           })
           :
           <></>
         }
+        {this.state.Reviews.length ?
+          this.state.Reviews.map((data, id) => {
+            return (
+              <>
+                <Container className="container">
+                  <Card style={{ width: '40rem' }}>
+                    <Card.Body>
+                      <Row>
+                        <Card.Title>💬 Reviews</Card.Title>
+                      </Row>
+
+                      {/* restaurant review */}
+                      <Row>
+                        <Col>
+                          <Card.Title>{data.storeName}</Card.Title>
+                          <Card.Text>{data.description}</Card.Text>
+                        </Col>
+                        <Col>
+
+                          {/* this button updates a review
+                          <Button
+                            variant="primary"
+                            onClick={this.showUpdateModalHandler}
+                          >
+                            <i className="fa fa-pencil"></i>
+                          </Button> */}
+
+                          {/* this button deletes a review */}
+                          <Button
+                            variant="primary"
+                            onClick={this.showDeleteModalHandler}
+                          >
+                            <i className="fa fa-trash-o"></i>
+                          </Button>
+                        </Col>
+                      </Row>
+                    </Card.Body>
+                  </Card>
+                </Container>
+              </>
+            )
+          })
+          :
+          <></>
+        }
+        {/* this modal confirms a delete */}
+        <Modal
+          show={this.state.showDelete}
+          onHide={this.hideDeleteModalHandler}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Are you sure you want to delete this?
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Button
+              variant="primary"
+              onClick={this.hideDeleteModalHandler}
+            > Yep!</Button>
+            <Button
+              variant="danger"
+              onClick={this.hideDeleteModalHandler}
+            > Nope!</Button>
+          </Modal.Body>
+        </Modal>
+
+        {/* this modal updates a review */}
+        <Modal
+          show={this.state.showUpdate}
+          onHide={this.hideUpdateModalHandler}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Update your review
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="formUpdate" controlId="formUpdateReview">
+                <Form.Label></Form.Label>
+                <Form.Control type="text" placeholder="REVIEW TEXT GOES HERE" />
+              </Form.Group>
+              <Modal.Footer>
+              <Button variant="primary">
+                Update
+              </Button>
+              </Modal.Footer>
+            </Form>
+          </Modal.Body>
+        </Modal>
+
+        {/* this modal shows an enlarged photo*/}
+        <Modal
+          show={this.state.showPhoto}
+          onHide={this.hidePhotoModalHandler}
+        >
+          <Modal.Body>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="http://placehold.jp/500x500.png"
+              // className='img-fluid'
+              />
+            </Card>
+          </Modal.Body>
+        </Modal>
       </>
     )
   }
 }
-
-//   renderPage = () => {
-//     return (
-//       <>
-//         <Card style={{ width: '40rem' }}>
-
-//           <Container>
-//             <Card.Header className="restaurantHeader">
-//               <Row>
-//                 <Col>
-//                   {/* restaurant photo */}
-//                   <Card.Img
-//                     variant="left"
-//                     src={this.state.FavesReviews[0].YelpData}
-//                     className="restPhoto"
-//                   />
-//                 </Col>
-//                 <Col>
-//                   {/* restaurant name */}
-//                   <Card.Title
-//                     className="restName">Taco Bell</Card.Title>
-
-//                   {/* restaurant address */}
-//                   <Card.Text className="restAddy">
-//                     <i className="fa fa-map-marker"></i> restaurant address
-//                   </Card.Text>
-//                 </Col>
-//                 <Col>
-//                   {/* this button shares*/}
-//                   <Button
-//                     variant="info"
-//                     className="share"
-//                   >
-//                     <i className="fa fa-share"></i>
-//                   </Button>
-
-//                   {/* this button deletes a restaurant */}
-//                   <Button
-//                     variant="dark"
-//                     onClick={this.showDeleteModalHandler}
-//                     className="deleteRest"
-//                   >
-//                     <i className="fa fa-trash-o"></i>
-//                   </Button>
-//                 </Col>
-//               </Row>
-//             </Card.Header>
-//           </Container>
-
-//           <Container>
-//             <Card.Body>
-//               <Row>
-//                 <Card.Title>💬 Reviews</Card.Title>
-//               </Row>
-//               <Row>
-//                 <Col>
-//                   <Card>
-//                     <Card.Text>
-//                       REVIEW GOES HERE
-//                     </Card.Text>
-//                   </Card>
-//                 </Col>
-//                 <Col>
-//                   {/* this button updates a review */}
-//                   <Button
-//                     variant="primary"
-//                     onClick={this.showUpdateModalHandler}
-//                   >
-//                     <i className="fa fa-pencil"></i>
-//                   </Button>
-//                   {/* this button deletes a review */}
-//                   <Button
-//                     variant="primary"
-//                     onClick={this.showDeleteModalHandler}
-//                   >
-//                     <i className="fa fa-trash-o"></i>
-//                   </Button>
-//                 </Col>
-//               </Row>
-//             </Card.Body>
-//           </Container>
-
-//         </Card>
-//       </>
-//     )
-//   }
-
-//   render() {
-//     // if (this.props.auth0.isAuthenticated && this.state.notLoaded) {
-//     //   this.getFavesAndReviews();
-//     // }
-//     console.log(this.state.FavesReviews);
-//     return (
-//       <>
-//         {/* this modal confirms a delete */}
-//         <Modal
-//           className="img-responsive"
-//           show={this.state.showDelete}
-//           onHide={this.hideDeleteModalHandler}
-//         >
-//           <Modal.Header closeButton>
-//             <Modal.Title>Are you sure you want to delete this?
-//             </Modal.Title>
-//           </Modal.Header>
-//           <Modal.Body>
-//             <Button
-//               variant="primary"
-//               onClick={this.hideDeleteModalHandler}
-//             > Yep!</Button>
-//             <Button
-//               variant="danger"
-//               onClick={this.hideDeleteModalHandler}
-//             > Nope!</Button>
-//           </Modal.Body>
-//         </Modal>
-
-//         {/* this modal updates a review */}
-//         <Modal
-//           className="img-responsive"
-//           show={this.state.showUpdate}
-//           onHide={this.hideUpdateModalHandler}
-//         >
-//           <Modal.Header closeButton>
-//             <Modal.Title>Update your review
-//             </Modal.Title>
-//           </Modal.Header>
-//           <Modal.Body>
-//             <Form>
-//               <Form.Group className="formUpdate" controlId="formUpdateReview">
-//                 <Form.Label>RESTAURANT NAME/OLD REVIEW TEXT?</Form.Label>
-//                 <Form.Control type="text" placeholder="REVIEW TEXT GOES HERE" />
-//               </Form.Group>
-//               <Button variant="primary">
-//                 Update
-//               </Button>
-//             </Form>
-//           </Modal.Body>
-//         </Modal>
-
-//         {/* this modal shows an enlarged photo*/}
-//         <Modal
-//           className="img-responsive"
-//           show={this.state.showPhoto}
-//           onHide={this.hidePhotoModalHandler}
-//         >
-//           <Modal.Body>
-//             <Card>
-//               <Card.Img
-//                 variant="top"
-//                 src="http://placehold.jp/500x500.png"
-//               // className='img-fluid'
-//               />
-//             </Card>
-//           </Modal.Body>
-//         </Modal>
-
-//         <Button
-//           onClick={this.getFavesAndReviews}
-//         >Show My Faves!</Button>
-//       </>
-//     )
-//   }
-// }
 
 export default withAuth0(Faves);
